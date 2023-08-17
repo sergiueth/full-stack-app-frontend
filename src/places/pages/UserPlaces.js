6f688db03ba5f256f1c4b412b71902e91b32f179
Mikoshi Kaneko <108369953+sergbucur@users.noreply.github.com>
2023-08-16 18:30:46 -0700

    pushed

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -32,7 +34,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
